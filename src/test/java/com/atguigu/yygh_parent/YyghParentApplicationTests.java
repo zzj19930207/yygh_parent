e7d07575f748b91784e43cdbcda7569aa8bfb294
zzj19930207 <zzj20181219@163.com>
2021-03-25 22:42:42 +0800

    预约挂号项目父工程创建

---
+++ src/test/java/com/atguigu/yygh_parent/YyghParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.yygh_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
